e45f885eec525eb66804f24d019784d4b681b318
Nazar <kryvyynazariy@gmail.com>
2017-11-28 19:40:25 +0200

    test

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -14,6 +14,7 @@ var About = (props) =>{
     return (
         <div>
             <h1 className="text-center page-title">About</h1>
+            
             <p>This is a weather  application build on React. I have built this for The Compleate React Web App Developer  Course.</p>
             <p>Here are some of the tools I used: </p>
             <ul>
