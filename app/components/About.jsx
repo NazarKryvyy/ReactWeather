ab2bcdd277a74d6b9ac3360f443e507447cfa320
Nazar <kryvyynazariy@gmail.com>
2017-11-29 12:54:50 +0200

    fix

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -22,7 +22,7 @@ var About = (props) =>{
                     <a href="https://facebook.github.io/react">React</a> - This is the Java Script framework used.
                 </li>
                 <li>
-                    <a href="http://openweathermap.org">Open Weathe Map</a> - I used Open Weathe Map to search for weather data by city name. 
+                    <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name. 
                 </li>
             </ul>
             
